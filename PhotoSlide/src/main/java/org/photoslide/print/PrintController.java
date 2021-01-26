eecf61f8a78ba5cbdadc068cf461be08eaa0f64c
lanthale <lanthale@macbook-pro-2.lanthaler.cc>
2021-01-26 21:47:50 +0100

    UI for the printer dialog designed

--- PhotoSlide/src/main/java/org/photoslide/print/PrintController.java
+++ PhotoSlide/src/main/java/org/photoslide/print/PrintController.java
@@ -9,8 +9,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
 import org.photoslide.ThreadFactoryPS;
 
 /**
@@ -22,11 +32,41 @@ public class PrintController implements Initializable {
     private ExecutorService executor;
     private ExecutorService executorParallel;    
     private DialogPane dialogPane;
+    @FXML
+    private ComboBox<?> printerCombo;
+    @FXML
+    private ComboBox<?> paperSizeCombo;
+    @FXML
+    private TextField copyQTY;
+    @FXML
+    private CheckBox greyscaleChoice;
+    @FXML
+    private RadioButton allPagesRadio;
+    @FXML
+    private RadioButton rangeRadio;
+    @FXML
+    private TextField pageFromText;
+    @FXML
+    private TextField pageToText;
+    @FXML
+    private ComboBox<?> copyiesPerPageCombo;
+    @FXML
+    private ToggleButton portraitButton;
+    @FXML
+    private ToggleButton landscapeButton;
+    @FXML
+    private Pagination paganinationControl;
+    @FXML
+    private ImageView previewPane;
+    private ToggleGroup tGroup;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {        
         executor = Executors.newSingleThreadExecutor(new ThreadFactoryPS("SearchToolExecutor"));
         executorParallel = Executors.newCachedThreadPool(new ThreadFactoryPS("SearchToolExecutor"));        
+        tGroup=new ToggleGroup();
+        portraitButton.setToggleGroup(tGroup);
+        landscapeButton.setToggleGroup(tGroup);
     }
 
     public void shutdown() {
@@ -36,6 +76,22 @@ public class PrintController implements Initializable {
 
     public void setDialogPane(DialogPane pane) {
         this.dialogPane = pane;        
+    }
+
+    @FXML
+    private void allPagesRadioAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void rangeRadioAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void pageFormTextAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void pageToTextAction(ActionEvent event) {
     }
 
     

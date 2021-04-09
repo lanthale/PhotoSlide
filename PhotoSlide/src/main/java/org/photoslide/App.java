769f132d8d8cdc72b5d9c1b41dcb2b6617729953
lanthale <lanthale@macbook-pro-2.lanthaler.cc>
2021-04-09 14:11:46 +0200

    Removed librawfx because temp filesystem is no longer supported in OSX

--- PhotoSlide/src/main/java/org/photoslide/App.java
+++ PhotoSlide/src/main/java/org/photoslide/App.java
@@ -23,14 +23,12 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import javafx.application.Preloader.ProgressNotification;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Paint;
 import javafx.stage.WindowEvent;
 import javax.imageio.spi.IIORegistry;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.spi.ServiceRegistry;
 import org.h2.fulltext.FullText;
-import org.librawfx.RAWImageLoaderFactory;
 import org.photoslide.datamodel.customformats.psdsupport.PSDImageLoaderFactory;
 import org.photoslide.datamodel.customformats.tiffsupport.TIFFImageLoaderFactory;
 import org.photoslide.pspreloader.PSPreloader;
@@ -91,7 +89,7 @@ public class App extends Application {
         notifyPreloader(new ProgressNotification(0.7));        
         TIFFImageLoaderFactory.install();
         PSDImageLoaderFactory.install();
-        RAWImageLoaderFactory.install();
+        //RAWImageLoaderFactory.install();
         notifyPreloader(new ProgressNotification(0.8));
     }
 
